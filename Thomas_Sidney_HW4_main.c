e7b560d56f28011f722f1a5dc2dbd564090155c3
Sidney <sid.14boys@yahoo.fr>
2023-10-05 16:22:26 -0700

    Thread reading file chunk + counting words on going

--- Thomas_Sidney_HW4_main.c
+++ Thomas_Sidney_HW4_main.c
@@ -6,6 +6,10 @@
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
+
+#define MAX_WORD_LENGTH 100
+#define MAX_TOTAL_WORDS 100000
 
 // You may find this Useful
 char *delim = "\"\'.“”‘’?:;-,—*($%)! \t\n\x0A\r";
@@ -13,18 +17,22 @@ char *delim = "\"\'.“”‘’?:;-,—*($%)! \t\n\x0A\r";
 //  mutex for thread synchronization
 pthread_mutex_t mutex;
 
-// * DATA STRUCTURE
+// * DATA STRUCTURES
 // structure to hold word counts
 typedef struct
 {
-    char word[100];
+    char word[MAX_WORD_LENGTH];
     int count;
 } WordCount;
+
+WordCount wordCount[MAX_TOTAL_WORDS];
+int numWords = 0; // track snumber of words in the wordCount array
 
 // structure to hold thread-specific data
 typedef struct
 {
     int fileDescriptor;
+    int threadIndex;
     off_t segmentSize;
 } ThreadData;
 
@@ -33,6 +41,50 @@ typedef struct
 void *countWords(void *arg)
 {
     ThreadData *threadData = (ThreadData *)arg;
+    int fileDescriptor = threadData->fileDescriptor;
+    int threadIndex = threadData->threadIndex;
+    off_t segmentSize = threadData->segmentSize;
+
+    printf("Thread %d started\n", threadIndex);
+
+    // calculate the start position for this thread
+    off_t startPosition = threadIndex * segmentSize;
+
+    printf("Thread %d start position: %lld\n", threadIndex, (long long)startPosition);
+
+    // seek to the start position in the file
+    lseek(fileDescriptor, startPosition, SEEK_SET);
+
+    printf("Thread %d seeked to position: %lld\n", threadIndex, (long long)startPosition);
+
+    // read the assigned portion of the file into a buffer
+    char buffer[segmentSize];
+    ssize_t bytesRead = read(fileDescriptor, buffer, segmentSize);
+
+    if (bytesRead < 0)
+    {
+        perror("Error reading file");
+        // handle the error
+    }
+
+    printf("Thread %d read %zd bytes from file\n", threadIndex, bytesRead);
+
+    // initialize strtok with delimiters
+    char *token = strtok(buffer, delim);
+
+    while (token != NULL)
+    {
+        printf("Thread %d: Token: %s\n", threadIndex, token);
+
+        // TODO: check if 'token' has 6 or more characters and update word counts accordingly
+        if (strlen(token) >= 6)
+        {
+            printf("Thread %d: Word has 6 characters\n", threadIndex);
+        }
+
+        // get the next token
+        token = strtok(NULL, delim);
+    }
 
     // lock mutex before any data shared
     pthread_mutex_lock(&mutex);
@@ -41,6 +93,8 @@ void *countWords(void *arg)
 
     // allow other threads to access shared data
     pthread_mutex_unlock(&mutex);
+
+    printf("Thread %d completed\n", threadIndex);
 
     pthread_exit(NULL);
 }
@@ -114,6 +168,9 @@ int main(int argc, char *argv[])
         threadDataArray[i].segmentSize = segmentSize;
         printf("Thread[%d].segmentSize: %lld\n", i, (long long)segmentSize);
 
+        // pass the thread index as an argument
+        threadDataArray[i].threadIndex = i; // set the thread index
+
         // create thread to process with countWords function
         pthread_create(&threads[i], NULL, countWords, &threadDataArray[i]);
     }
@@ -127,8 +184,15 @@ int main(int argc, char *argv[])
     }
     printf("--- END WAIT THREADS TO FINISH ---\n\n");
 
+    // TODO: sort the wordCount array based on word counts
+    // TODO: use qsort
+    // TODO: sort it in descending order so that the top words come first
     // * DISPLAY TOP 10 WORDS
     // TODO: process TOP 10 and display
+    for (int i = 0; i < 10; i++)
+    {
+        // printf("Number %d is %s with a count of %d\n", i + 1, wordCount[i].word, wordCount[i].count);
+    }
 
     //**************************************************************
     // DO NOT CHANGE THIS BLOCK
