54a22e9f439879f0ebdedd1d882e3abff5665ff5
Sidney <sid.14boys@yahoo.fr>
2023-10-07 13:37:14 -0700

    Refactoring + printf removed

--- Thomas_Sidney_HW4_main.c
+++ Thomas_Sidney_HW4_main.c
@@ -50,28 +50,18 @@ void *countWords(void *arg)
     off_t segmentSize = threadData->segmentSize;
     char *buffer = threadData->buffer;
 
-    printf("Thread %d started\n", threadIndex);
-
     // calculate the start position for this thread
     off_t startPosition = threadIndex * segmentSize;
-
-    printf("Thread %d start position: %lld\n", threadIndex, (long long)startPosition);
 
     // seek to the start position in the file
     lseek(fileDescriptor, startPosition, SEEK_SET);
 
-    // Print the current file pointer position before reading
-    off_t currentPos = lseek(fileDescriptor, 0, SEEK_CUR);
-    printf("Thread %d current file pointer position: %lld\n", threadIndex, (long long)currentPos);
-
-    printf("Thread %d seeked to position : %lld\n\n", threadIndex, (long long)startPosition);
+    // off_t currentPos = lseek(fileDescriptor, 0, SEEK_CUR);
 
     // read the assigned portion of the file into a buffer
     ssize_t bytesRead = read(fileDescriptor, buffer, segmentSize);
 
-    // print the new file pointer position after reading
-    currentPos = lseek(fileDescriptor, 0, SEEK_CUR);
-    printf("Thread %d new file pointer position: %lld\n\n", threadIndex, (long long)currentPos);
+    // currentPos = lseek(fileDescriptor, 0, SEEK_CUR);
 
     if (bytesRead < 0)
     {
@@ -79,16 +69,11 @@ void *countWords(void *arg)
         pthread_exit(NULL);
     }
 
-    // printf("Segment Size: %lld\n", (long long)segmentSize);
-    // printf("Buffer size: %lu\n\n", sizeof(buffer));
-
-    printf("Thread %d read %zd bytes from file\n", threadIndex, bytesRead);
-
-    // Check if the segment size is greater than the bytes actually read
-    if (segmentSize > bytesRead)
-    {
-        fprintf(stderr, "Thread %d: Segment size is larger than bytes read. Expected %lld bytes, but read %zd bytes.\n", threadIndex, (long long)segmentSize, bytesRead);
-    }
+    // check if the segment size is greater than the bytes actually read
+    // if (segmentSize > bytesRead)
+    // {
+    //     fprintf(stderr, "Thread %d: Segment size is larger than bytes read. Expected %lld bytes, but read %zd bytes.\n", threadIndex, (long long)segmentSize, bytesRead);
+    // }
 
     // initialize strtok_r with delimiters for tokenisation
     char *token;
@@ -98,12 +83,9 @@ void *countWords(void *arg)
 
     while (token != NULL)
     {
-        printf("Thread %d: Token: %s\n", threadIndex, token);
 
         if (strlen(token) >= 6)
         {
-            printf("Thread %d -- %s has 6 characters or more\n", threadIndex, token);
-
             // lock mutex before any data shared
             pthread_mutex_lock(&mutex);
 
@@ -136,8 +118,6 @@ void *countWords(void *arg)
         token = strtok_r(NULL, delim, &saveptr);
     }
 
-    printf("Thread %d completed\n", threadIndex);
-
     pthread_exit(NULL); // exit the thread
 }
 
@@ -167,32 +147,26 @@ int main(int argc, char *argv[])
 
     // parse command-line arguments
     char *fileName = argv[1];
-    printf("File Name: %s\n", fileName);
     int threadCount = atoi(argv[2]);
-    printf("Thread Count: %d\n", threadCount);
 
     // * FILE OPENING
-    printf("--- START READING FILE ---\n");
     // open the file
     int fileDescriptor = open(fileName, O_RDONLY);
-    printf("File Descriptor: %d\n", fileDescriptor);
     if (fileDescriptor == -1)
     {
         fprintf(stderr, "Failed to open file '%s' for reading: %s\n", fileName, strerror(errno));
         exit(EXIT_FAILURE);
     }
-    printf("--- END READING FILE ---\n\n");
 
     // * FILE SIZE & THREAD SEGMENTATION
     // determine the file size
     off_t fileSize = lseek(fileDescriptor, 0, SEEK_END);
-    printf("Size of the file: %lld\n", (long long)fileSize);
+
     // reset file pointer to beginning of file
     lseek(fileDescriptor, 0, SEEK_SET);
 
     // calculate the segment size for each thread
     off_t segmentSize = fileSize / threadCount;
-    printf("Size for each thread: %lld\n\n", (long long)segmentSize);
 
     // initialize the mutex
     pthread_mutex_init(&mutex, NULL);
@@ -217,17 +191,14 @@ int main(int argc, char *argv[])
     // * THREADS CREATION & EXECUTION
     ThreadData *threadDataArray = malloc(threadCount * sizeof(ThreadData));
 
-    printf("--- START CREATE & START THREADS ---\n");
     // create and start threads
     for (int i = 0; i < threadCount; i++)
     {
         // ensure thread know which file to read
         threadDataArray[i].fileDescriptor = fileDescriptor;
-        printf("Thread[%d].fileDescriptor: %d\n", i, fileDescriptor);
 
         // ensure thread know which file segment to process
         threadDataArray[i].segmentSize = segmentSize;
-        printf("Thread[%d].segmentSize: %lld\n", i, (long long)segmentSize);
 
         // pass the thread index as an argument
         threadDataArray[i].threadIndex = i; // set the thread index
@@ -248,19 +219,16 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
     }
-    printf("--- END CREATE & START THREADS ---\n\n");
 
-    printf("--- START WAIT THREADS TO FINISH ---\n");
     // wait for the threads to finish
     for (int i = 0; i < threadCount; i++)
     {
         pthread_join(threads[i], NULL);
-        if (pthread_join(threads[i], NULL) != 0)
-        {
-            fprintf(stderr, "Error: Failed to join thread %d: %s\n", i, strerror(errno));
-        }
+        // if (pthread_join(threads[i], NULL) != 0)
+        // {
+        //     fprintf(stderr, "Error: Failed to join thread %d: %s\n", i, strerror(errno));
+        // }
     }
-    printf("--- END WAIT THREADS TO FINISH ---\n\n");
 
     free(threadDataArray);
 
@@ -272,7 +240,8 @@ int main(int argc, char *argv[])
 
     // sort the wordCount array
     qsort(wordCount, numWords, sizeof(WordCount), compareWordCounts);
-    printf("numWords : %d\n", numWords);
+
+    printf("\n\n");
 
     printf("Word Frequency Count on %s with %d threads\n", fileName, threadCount);
     // * DISPLAY TOP 10 WORDS
